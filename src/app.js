10f4cbd7710f63a6f4001ee1c1f0701baf748f44
Matheus Porto <matheus.porto@sap.com>
2023-09-11 17:36:25 -0300

    Add cors

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ mongoose.connect(process.env.MONGODB_URI, {
 const express = require('express');
 const app = express();
 
+const cors = require('cors'); 
+app.use(cors());
 app.use(express.json());
 
 const routes = require('./routes');
